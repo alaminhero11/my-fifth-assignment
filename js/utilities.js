913f5e85d2f2b223b90e8168a2f76ac24a1c6e3f
alaminhero11 <alaminhero55@gmail.com>
2024-09-23 21:58:20 +0600

    my first commit

---
+++ js/utilities.js
@@ -0,0 +1,32 @@
+// --------------------Input-Add-Money-Function-------------------
+
+function getInputFieldValueById(id) {
+  const InputAddBalance = document.getElementById(id).value;
+  const newInputAddBalance = parseFloat(InputAddBalance);
+  return newInputAddBalance;
+}
+
+// ---------------------Current-Balance-Function------------------
+
+function getTextFieldValueById(id) {
+  const currentBalance = document.getElementById(id).innerText;
+  const currentAmount = parseFloat(currentBalance);
+  return currentAmount;
+}
+
+// ------------------------------Hide-----------------------------
+
+function showSectionById(id) {
+  document.getElementById("main-section").classList.add("hidden");
+  document.getElementById("transaction-section").classList.add("hidden");
+  // --------------
+  document.getElementById(id).classList.remove("hidden");
+}
+
+// ---------------------Available-Balance-Function----------------
+
+// function getTextFieldValueById(id) {
+//   const availableBalance = document.getElementById(id).innerText;
+//   const newAvailableBalance = parseFloat(availableBalance);
+//   return newAvailableBalance;
+// }
