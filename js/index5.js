0dcd645547afcc9f14781d0f1f6de07ea0fb09cc
alaminhero11 <alaminhero55@gmail.com>
2024-09-24 12:13:55 +0600

    added all content

--- js/index5.js
+++ js/index5.js
@@ -1,4 +1,8 @@
-// -----------------------------------------------------------------------------------------
+// -----------------------------Input-Donation-Function---------------------------------------------
+document.getElementById("blog-button").addEventListener("click", function () {
+  window.location.href = "blog.html";
+});
+// -----------------------------Input-Donation-Function---------------------------------------------
 const inputDonationButton = document.getElementById("input-donation-button");
 inputDonationButton.addEventListener("click", function (event) {
   event.preventDefault();
@@ -15,10 +19,32 @@ inputDonationButton.addEventListener("click", function (event) {
 
   const newAddBalance = availableBalance - inputAddBalance;
   document.getElementById("available-balance").innerText = newAddBalance;
+  document.getElementById(
+    "transaction-container"
+  ).innerText = `${inputAddBalance} Taka is Donated for Donate for Flood at Noakhali, Bangladesh.`;
 
   const newBalance = currentBalance + inputAddBalance;
   document.getElementById("current-balance").innerText = newBalance;
 });
 
-// -----------------------------------------------------------------------------------------
+// --------------------------------------Button-Function--------------------------------------------
+// ---Donation-History-Click-Button---
+const historyButton = document.getElementById("history-button");
+const donationButton = document.getElementById("donation-button");
+// ---History-Button---
+historyButton.addEventListener("click", function () {
+  historyButton.classList.add("bg-secondary");
+  donationButton.classList.remove("bg-secondary");
+
+  document.getElementById("main-section").classList.add("hidden");
+  document.getElementById("transaction-section").classList.remove("hidden");
+});
+// ---Donation-Button---
+donationButton.addEventListener("click", function () {
+  donationButton.classList.add("bg-secondary");
+  historyButton.classList.remove("bg-secondary");
+
+  document.getElementById("transaction-section").classList.add("hidden");
+  document.getElementById("main-section").classList.remove("hidden");
+});
 // -----------------------------------------------------------------------------------------
